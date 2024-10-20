5c302ab1de51d068c030cb6a4ab7ff7f873d8391
ValiQ code <bquast@valiq.com>
2024-10-20 22:10:30 +0200

    don't throw error, print warning in console

--- index_files/ff5b474.modern.js
+++ index_files/ff5b474.modern.js
@@ -145,9 +145,10 @@
             return Object.prototype.hasOwnProperty.call(object, e);
         }),
         (f.p = "/"),
-        // (f.oe = function (e) {
-        //     throw (console.error(e), e);
-        // });
+        f.oe = function (e) {
+            console.warn('Chunk loading error:', e);
+            // Don't throw the error
+        };
     var d = (window.webpackJsonp = window.webpackJsonp || []),
         l = d.push.bind(d);
     (d.push = r), (d = d.slice());
