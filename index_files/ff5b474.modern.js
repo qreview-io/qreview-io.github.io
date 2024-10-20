0db0a3882bc9060dc6ba35d5577f021448d91721
ValiQ code <bquast@valiq.com>
2024-10-20 22:17:22 +0200

    Update ff5b474.modern.js

--- index_files/ff5b474.modern.js
+++ index_files/ff5b474.modern.js
@@ -143,10 +143,9 @@
             return Object.prototype.hasOwnProperty.call(object, e);
         }),
         (f.p = "/"),
-        f.oe = function (e) {
-            console.warn('Chunk loading error:', e);
-            // Don't throw the error
-        };
+        (f.oe = function (e) {
+            throw (console.error(e), e);
+        });
     var d = (window.webpackJsonp = window.webpackJsonp || []),
         l = d.push.bind(d);
     (d.push = r), (d = d.slice());
