fd01241399d1ad12b058d0329c1512fe634b3165
ValiQ code <bquast@valiq.com>
2024-10-20 21:58:27 +0200

    try this suggestion, not sure why

--- index_files/ff5b474.modern.js
+++ index_files/ff5b474.modern.js
@@ -144,7 +144,7 @@
         (f.o = function (object, e) {
             return Object.prototype.hasOwnProperty.call(object, e);
         }),
-        (f.p = "./_nuxt/"),
+        (f.p = "/"),
         (f.oe = function (e) {
             throw (console.error(e), e);
         });
